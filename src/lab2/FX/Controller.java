d8e285954dfff46f3c3978a04a65616547422358
cenjoyer13 <lysenkosun20@gmail.com>
2024-05-09 04:06:00 +0300

    lab5

---
+++ src/lab2/FX/Controller.java
@@ -0,0 +1,94 @@
+package lab2.FX;
+
+import lab2.main.*;
+
+import java.text.NumberFormat;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.chart.*;
+import javafx.scene.control.*;
+import javafx.stage.*;
+
+public class Controller {
+
+	double a = -0.7;
+	double b = 0.3;
+	double x = 2;
+
+	final Function expression2 = Multiplication.of(X.x(),Exponential.of(Cos.of(Exponential.of(Abs.of(Sum.of(Multiplication.of(Const.of(a),X.x()), Const.of(b))), 3)), 2));
+	final Function expression1 = Sum.of(Multiplication.of(Exponential.of(
+			Sinh.of(X.x()),3), Const.of(b)), Multiplication.of(Const.of(-1),Abs.of(Cosh.of(Exponential.of(Sum.of(X.x(), Const.of(a)), 2.5)))));
+
+	Function inUse;
+
+    public RadioButton rb1;
+    public RadioButton rb2;
+    ToggleGroup toggleGroup;
+
+    public Label calc;
+    public Label der;
+    public TextField Xin;
+    public TextField start;
+    public TextField fin;
+
+    @FXML
+    public void initialize() {
+    	toggleGroup = new ToggleGroup();
+       rb1.setToggleGroup(toggleGroup);
+       rb2.setToggleGroup(toggleGroup);
+       inUse=expression1;
+    }
+
+    @FXML
+    public void Select(ActionEvent actionEvent) {
+    	if ("rb1" == ((Node) toggleGroup.getSelectedToggle()).getId()) inUse=expression1;
+    	else inUse=expression2;
+    }
+
+    @FXML
+    public void Calculate(ActionEvent actionEvent) {
+    	x = Double.parseDouble(Xin.getText());
+    	calc.setText("f(" + x + "): " + inUse.calculate(x));
+    }
+
+    @FXML
+    public void Derivative(ActionEvent actionEvent) {
+    	x = Double.parseDouble(Xin.getText());
+    	der.setText("f`(" + x + "): " + inUse.derivative().calculate(x));
+    }
+
+
+    public LineChart<String, Double> chart;
+    @FXML
+    public void createChart(ActionEvent actionEvent) {
+        final XYChart.Series<String, Double> series = new XYChart.Series<>();
+        series.setName(null);
+        final XYChart.Series<String, Double> series1 = new XYChart.Series<>();
+        series1.setName(null);
+        final XYChart.Series<String, Double> series2 = new XYChart.Series<>();
+        series2.setName(null);
+
+        final int beg = Integer.parseInt(start.getText());
+        final int end = Integer.parseInt(fin.getText());
+
+        for (int i = beg*10; i < end*10; i++) {
+            final double x = (double) i / 10;
+            final double y = inUse.calculate(x);
+            final double y1 = inUse.derivative().calculate(x);
+            final double y2 = inUse.derivative().derivative().calculate(x);
+            series.getData().add(new XYChart.Data<>(String.format("%.2f", x), y));
+            series1.getData().add(new XYChart.Data<>(String.format("%.2f", x), y1));
+            series2.getData().add(new XYChart.Data<>(String.format("%.2f", x), y2));
+        }
+        chart.setCreateSymbols(false); // remove all dots
+        chart.getData().clear();
+        chart.getData().add(series);
+        chart.getData().add(series1);
+        chart.getData().add(series2);
+    }
+}
+
--- src/lab2/test/AbsTest.java
+++ src/lab2/test/AbsTest.java
@@ -1,69 +1,66 @@
 package lab2.test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.Assert.*;
 import java.text.NumberFormat;
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import lab2.main.*;
 
-class AbsTest {
+public class AbsTest {
 
 	@Test
-	void EqualsDifClassTest() {
+	public void EqualsDifClassTest() {
 		Function f1 = Abs.of(Const.of(1));
 		Function f2 = Cos.of(Const.of(1));
 		assertFalse(f1.equals(f2));
 	}
 	@Test
-	void EqualsSimFunctions() {
+	public void EqualsSimFunctions() {
 		Function f1 = Abs.of(Const.of(1));
 		Function f2 = Abs.of(Const.of(1));
 		assertTrue(f1.equals(f2));
 	}
 	@Test
-	void EqualsSimilarFunctions() {
+	public void EqualsSimilarFunctions() {
 		Function f1 = Abs.of(Cos.of(Const.of(1)));
 		Function f2 = Abs.of(Cos.of(Const.of(1)));
 		assertTrue(f1.equals(f2));
 	}
-	
+
 	@Test
-	void CalculateSimple(){
+	public void CalculateSimple(){
 		Function f = Abs.of(X.x());
 		assertTrue(f.calculate(-2)==2);
 	}
-	
+
 	@Test
-	void CalculateComplex(){
+	public void CalculateComplex(){
 		Function f = Abs.of(Exponential.of(X.x(),3));
 		assertTrue(f.calculate(-2)==8);
 	}
-	
+
 	@Test
-	void DeriviateSimple(){
+	public void DeriviateSimple(){
 		Function f = Abs.of(X.x());
 		Function fder = Multiplication.of(X.x(), Exponential.of(Abs.of(X.x()), -1), Const.of(1));
 		assertTrue(f.derivative().equals(fder));
 	}
-	
+
 	@Test
-	void DeriviateComplex(){
+	public void DeriviateComplex(){
 		Function f = Abs.of(Sin.of(X.x()));
 		Function fder = Multiplication.of(Sin.of(X.x()), Exponential.of(Abs.of(Sin.of(X.x())), -1), Multiplication.of(Cos.of(X.x()), Const.of(1)));
 		assertTrue(f.derivative().equals(fder));
 	}
-	
+
 	@Test
-	void StringSimple(){
+	public void StringSimple(){
 		final NumberFormat nf = NumberFormat.getInstance();
 		Function f = Abs.of(X.x());
 		assertTrue(f.toPrettyString(nf).equals("|x|"));
 	}
 
 	@Test
-	void StringComplex(){
+	public void StringComplex(){
 		final NumberFormat nf = NumberFormat.getInstance();
 		Function f = Abs.of(Sin.of(X.x()));
 		assertTrue(f.toPrettyString(nf).equals("|sin(x)|"));
